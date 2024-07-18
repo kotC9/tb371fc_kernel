10d6c4e1ecc76b9977c7474494073f96de1503ce
kotC9 <krot.ik@ya.ru>
2024-07-18 13:44:23 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Сборка
+
+на linux машине(например debian 12 или ubuntu 24.04)
+
+./build.sh
+
+либо собирать через devcontainer, необходим докер
+
+полученное ядро в ./build/kernel/arch/arm64/boot файл Image
+полученные модули в ./build/vendor
+
---
+++ build.sh
@@ -0,0 +1,20 @@
+set -e 
+set -o pipefail
+
+PROJECT=tb371fc
+CONFIG=vendor/${PROJECT}_defconfig
+GCC_PATH=${PWD}/prebuilts/aarch64-linux-android-4.9/bin
+CC=${PWD}/prebuilts/clang-r353983c/bin/clang
+export PATH=$GCC_PATH:$PATH
+
+
+BUILD_PATH=${PWD}/build
+KERNEL_BUILD_PATH=${BUILD_PATH}/kernel
+MOD_PATH=${BUILD_PATH}/vendor
+
+mkdir -p build
+
+make -j40 -C kernel O=${KERNEL_BUILD_PATH} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- CLANG_TRIPLE=aarch64-linux-gnu- CC=${CC} ${CONFIG}
+make -j40 V=1 -C kernel O=${KERNEL_BUILD_PATH} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- CLANG_TRIPLE=aarch64-linux-gnu- CC=${CC}
+make -j40 -C kernel O=${KERNEL_BUILD_PATH} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- CLANG_TRIPLE=aarch64-linux-gnu- CC=${CC} modules
+make -j40 -C kernel O=${KERNEL_BUILD_PATH} ARCH=arm64 CROSS_COMPILE=aarch64-linux-android- CLANG_TRIPLE=aarch64-linux-gnu- CC=${CC} modules_install INSTALL_MOD_PATH=${MOD_PATH}
